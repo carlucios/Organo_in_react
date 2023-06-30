e1cda1a683fec18af9d35063136d9c3f60833730
carlucios <carlucios@gmail.com>
2023-06-30 11:47:31 -0300

    Limpando arquivos não utilizados

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import Banner from './componentes/Banner/Banner';
+import Banner from './components/Banner/Banner';
+import TextField from './components/TextField/TextField';
 
 function App() {
   return (
     <div className="App">
       <Banner />
+      <TextField label="Nome" placeholder="Digite o seu nome..."/>
+      <TextField label="Cargo" placeholder="Digite o seu cargo..."/>
+      <TextField label="Imagem" placeholder="Digite o endereço da sua imagem..."/>
+      <TextField label=""/>
     </div>
   );
 }
